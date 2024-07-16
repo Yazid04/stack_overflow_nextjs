fc1b48f1d264fffb957171847e5f1e8200b61a1b
Yazid <yazidramadan04@gmail.com>
2024-07-16 19:56:41 +0700

    add assets and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,30 @@ import {
 } from "@clerk/nextjs";
 import React from "react";
 
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
 import "../globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "DevFlow",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaboration with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more!",
+  icons: {
+    icon: "/assets/images/site-logo.svg", // Corrected path
+  },
+};
 
 export default function RootLayout({
   children,
@@ -15,15 +38,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimry: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>
+        <head>
+          <title>{metadata.title}</title>
+          <link rel="icon" href={metadata.icons.icon} type="image/svg+xml" />
+          <meta name="description" content={metadata.description} />
+        </head>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           <SignedOut>
             <SignInButton />
           </SignedOut>
           <SignedIn>
             <UserButton />
           </SignedIn>
+          <h1 className="">This is a text</h1>
           {children}
         </body>
       </html>
