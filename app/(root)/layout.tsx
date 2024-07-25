18f47c27e743b202efab3540e99adecb77c81a3b
Yazid <yazidramadan04@gmail.com>
2024-07-25 11:07:32 +0700

    implemented left & right sidebars

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,17 +3,19 @@ import React from 'react'
 
 import '../../globals.css'
 import '../../styles/theme.css'
+import LeftSideBar from '@/components/shared/LeftSideBar'
+import RightSideBar from '@/components/shared/RightSideBar'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="background-light850_dark100 relative">
       <Navbar />
       <div className="flex">
-        LeftSideBar
+        <LeftSideBar />
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
-        RightSideBar
+        <RightSideBar />
       </div>
       Toaster
     </main>
