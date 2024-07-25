675a9e1daba775e6e6a04597bac4632a7ef04410
Yazid <yazidramadan04@gmail.com>
2024-07-25 10:24:08 +0700

    implemented navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,46 +4,46 @@ import {
   SignedIn,
   SignedOut,
   UserButton,
-} from "@clerk/nextjs";
-import React from "react";
+} from '@clerk/nextjs'
+import React from 'react'
 
-import { Inter, Space_Grotesk } from "next/font/google";
-import type { Metadata } from "next";
-import "../globals.css";
-import { ThemeProvider } from "@/context/ThemeProvider";
+import { Inter, Space_Grotesk } from 'next/font/google'
+import type { Metadata } from 'next'
+import '../globals.css'
+import { ThemeProvider } from '@/context/ThemeProvider'
 
 const inter = Inter({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  variable: "--font-inter",
-});
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter',
+})
 
 const spaceGrotesk = Space_Grotesk({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600"],
-  variable: "--font-spaceGrotesk",
-});
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600'],
+  variable: '--font-spaceGrotesk',
+})
 
 export const metadata: Metadata = {
-  title: "DevFlow",
+  title: 'DevFlow',
   description:
-    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaboration with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more!",
+    'A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaboration with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more!',
   icons: {
-    icon: "/assets/images/site-logo.svg", // Corrected path
+    icon: '/assets/images/site-logo.svg', // Corrected path
   },
-};
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <ClerkProvider
       appearance={{
         elements: {
-          formButtonPrimry: "primary-gradient",
-          footerActionLink: "primary-text-gradient hover:text-primary-500",
+          formButtonPrimry: 'primary-gradient',
+          footerActionLink: 'primary-text-gradient hover:text-primary-500',
         },
       }}
     >
@@ -54,17 +54,19 @@ export default function RootLayout({
           <meta name="description" content={metadata.description} />
         </head>
         <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
-          <ThemeProvider>
-            <SignedOut>
+          <ThemeProvider>{children}</ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
+
+/*
+This was right before the {children}
+<SignedOut>
               <SignInButton />
             </SignedOut>
             <SignedIn>
               <UserButton />
             </SignedIn>
-            {children}
-          </ThemeProvider>
-        </body>
-      </html>
-    </ClerkProvider>
-  );
-}
+*/
