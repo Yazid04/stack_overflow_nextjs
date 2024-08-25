0395ada9aabc3c24d72fa7596fc4047ece33c12e
Yazid <yazidramadan04@gmail.com>
2024-08-25 14:13:21 +0700

    Badges system implemented

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,8 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 import qs from 'query-string'
+import { BADGE_CRITERIA } from '@/constants'
+import { BadgeCounts } from '@/types'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -53,6 +55,18 @@ export const formatAndDivideNumber = (num: number): string => {
     return num.toString()
   }
 }
+
+export const getJoinedDate = (date: Date): string => {
+  // Extract the month and year from the Date object
+  const month = date.toLocaleString('default', { month: 'long' })
+  const year = date.getFullYear()
+
+  // Create the joined date string (e.g., "September 2023")
+  const joinedDate = `${month} ${year}`
+
+  return joinedDate
+}
+
 interface UrlQueryParams {
   params: string
   key: string
@@ -71,17 +85,6 @@ export const formUrlQuery = ({ params, key, value }: UrlQueryParams) => {
     },
     { skipNull: true }
   )
-}
-
-export const getJoinedDate = (date: Date): string => {
-  // Extract the month and year from the Date object
-  const month = date.toLocaleString('default', { month: 'long' })
-  const year = date.getFullYear()
-
-  // Create the joined date string (e.g., "September 2023")
-  const joinedDate = `${month} ${year}`
-
-  return joinedDate
 }
 
 interface RemoveUrlQueryParams {
@@ -106,4 +109,34 @@ export const removeKeysFromQuery = ({
     },
     { skipNull: true }
   )
+}
+
+interface BadgeParam {
+  criteria: {
+    type: keyof typeof BADGE_CRITERIA
+    count: number
+  }[]
+}
+
+export const assignBadges = (params: BadgeParam) => {
+  const badgeCounts: BadgeCounts = {
+    GOLD: 0,
+    SILVER: 0,
+    BRONZE: 0,
+  }
+
+  const { criteria } = params
+
+  criteria.forEach((item) => {
+    const { type, count } = item
+    const badgeLevels: any = BADGE_CRITERIA[type]
+
+    Object.keys(badgeLevels).forEach((level: any) => {
+      if (count >= badgeLevels[level]) {
+        badgeCounts[level as keyof BadgeCounts] += 1
+      }
+    })
+  })
+
+  return badgeCounts
 }
